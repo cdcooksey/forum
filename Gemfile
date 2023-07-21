7a492d314c2363c45805a691221a48f9b80e3674
Charlie Cooksey <cdcooksey@gmail.com>
2023-07-21 22:03:23 +0930

    cleanup

--- Gemfile
+++ Gemfile
@@ -62,3 +62,5 @@ group :test do
 end
 
 gem "tailwindcss-rails", "~> 2.0"
+
+gem "rubocop", "~> 1.50", :groups => []
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :set_post, only: %i[show edit update destroy]
 
   # GET /posts or /posts.json
   def index
@@ -10,13 +10,13 @@ class PostsController < ApplicationController
   def show
     respond_to do |format|
       format.html
-      format.turbo_stream {
+      format.turbo_stream do
         render turbo_stream: turbo_stream.replace(
           @post,
-          partial: "posts/post",
+          partial: 'posts/post',
           locals: { post: @post }
         )
-      }
+      end
     end
   end
 
@@ -29,13 +29,13 @@ class PostsController < ApplicationController
   def edit
     respond_to do |format|
       format.html
-      format.turbo_stream {
+      format.turbo_stream do
         render turbo_stream: turbo_stream.replace(
           @post,
-          partial: "posts/form",
+          partial: 'posts/form',
           locals: { post: @post }
         )
-      }
+      end
     end
   end
 
@@ -45,19 +45,21 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to post_url(@post), notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
         format.turbo_stream { redirect_to post_url(@post) }
+
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
-        format.turbo_stream {
+
+        format.turbo_stream do
           render turbo_stream: turbo_stream.replace(
             @post,
-            partial: "posts/form",
+            partial: 'posts/form',
             locals: { post: @post }
           )
-        }
+        end
       end
     end
   end
@@ -66,19 +68,19 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        format.html { redirect_to post_url(@post), notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
         format.turbo_stream { redirect_to post_url(@post) }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
-        format.turbo_stream {
+        format.turbo_stream do
           render turbo_stream: turbo_stream.replace(
             @post,
-            partial: "posts/form",
+            partial: 'posts/form',
             locals: { post: @post }
           )
-        }
+        end
       end
     end
   end
@@ -88,19 +90,20 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:title, :body, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
+  end
 end
